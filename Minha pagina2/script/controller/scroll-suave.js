f76c4ad3cfa5a26399e0c47ec4c81840a1edc7a0
miguelgui57 <82742898+miguelgui57@users.noreply.github.com>
2023-02-28 12:43:59 -0300

    Add files via upload

---
+++ Minha pagina2/script/controller/scroll-suave.js
@@ -0,0 +1,20 @@
+const linkInterno = document.querySelector('[data-scroll="suave"] a[href^="#"]')
+
+function ScrollTop(event){
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute('href');
+
+    const topo = document.querySelector(href);
+
+    topo.scrollIntoView({
+        behavior:'smooth',
+        block: 'start',
+    });
+
+
+
+    console.log(href);
+}
+
+linkInterno.addEventListener('click', ScrollTop)
